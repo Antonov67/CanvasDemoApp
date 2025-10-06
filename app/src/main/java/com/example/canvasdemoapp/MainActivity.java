e397dd412912805bfb1636fa36c724819d232db8
Dm Ant <Dm@Ant.ru>
2025-10-06 15:13:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/canvasdemoapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.canvasdemoapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(new MyDraw(this));
+
+    }
+}
+
+class MyDraw extends View{
+
+    public MyDraw(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawColor(Color.GRAY);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 200, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+
+        canvas.drawRect(100,100,800,500, paint);
+
+        paint.setTextSize(76f);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawText("Android", 100, 1500, paint);
+
+    }
+}
